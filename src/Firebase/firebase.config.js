22327fb66ec328ad46ccb95a3603497ad52bad6a
saidul-islam-max <mdsaidulislamjuned@gmail>
2021-10-15 11:38:51 +0600

    update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyA7EVjf0HMP1hLP7Gw8OQe-DgkUnhaWaZU",
+    authDomain: "ema-jhon-simple-d3548.firebaseapp.com",
+    projectId: "ema-jhon-simple-d3548",
+    storageBucket: "ema-jhon-simple-d3548.appspot.com",
+    messagingSenderId: "293751137471",
+    appId: "1:293751137471:web:b450c847fa25f7906b520a"
 };
 
 export default firebaseConfig;
